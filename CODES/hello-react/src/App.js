be29e29c8e03f2ad9cf33a48406ff311a576547b
lihongyao <lihy_online@163.com>
2022-04-23 21:26:52 +0800

    优化更新

--- CODES/hello-react/src/App.js
+++ CODES/hello-react/src/App.js
@@ -2,12 +2,13 @@
  * @Author: Lee
  * @Date: 2022-04-08 16:36:33
  * @LastEditors: Lee
- * @LastEditTime: 2022-04-22 17:08:15
+ * @LastEditTime: 2022-04-22 23:23:21
  */
 
 import Basic from './components/Basic';
 import State from './components/State';
 import IForm from './components/Form';
+import Promote from './components/Promote';
 const App = () => {
   return (
     <div className='App'>
@@ -15,6 +16,7 @@ const App = () => {
       <Basic />
       <State />
       <IForm />
+      <Promote />
     </div>
   );
 };
--- CODES/hello-react/src/components/Promote/index.jsx
+++ CODES/hello-react/src/components/Promote/index.jsx
@@ -2,6 +2,29 @@
  * @Author: Lee
  * @Date: 2022-04-18 16:17:32
  * @LastEditors: Lee
- * @LastEditTime: 2022-04-22 17:09:53
+ * @LastEditTime: 2022-04-22 23:33:30
  */
 
+import { Component } from 'react';
+import Input from './Input';
+export default class Controller extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: '',
+    };
+  }
+  handleChange(message) {
+    this.setState({
+      message
+    })
+  }
+  render() {
+    return (
+      <div style={{margin: '30px 0'}}>
+        <Input message={this.state.message} handleChange={this.handleChange.bind(this)}/>
+        <Input message={this.state.message} handleChange={this.handleChange.bind(this)}/>
+      </div>
+    );
+  }
+}
